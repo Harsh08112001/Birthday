4aacf316397ee45cf7a64d7a54cb7edd45191e7e
Harsh Solanki <68112461+Harsh08112001@users.noreply.github.com>
2026-01-11 14:41:42 +0530

    Update target date for countdown functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Target date: January 23, 2026 at 12:00 AM
 // For testing: set to past date to enable button immediately
-const targetDate = new Date('2026-01-04T00:00:00').getTime();
+const targetDate = new Date('2026-01-12T00:00:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
@@ -82,5 +82,6 @@ setInterval(updateCountdown, 1000);
 
 // Initial update
 updateCountdown();
+
 
 
